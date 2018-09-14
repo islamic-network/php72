294d94635c8104db1b4fa16d9833a00949e58787
Meezaan-ud-Din <meezaan@giffgaff.co.uk>
2018-09-14 11:18:58 +0100

    Added build file and vim for the dev version

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # PHP 7.2 Docker Image with Apache
 To use:
 ```
-docker build . -t php72
+docker build -f Dockerfile.dev . -t php72
 docker run -d -p 8080:80 -p 443:443 -it -v app:/app --name=php72 php72
 ```
+
+Remember:
+
+Document root in the container is /app/www.
---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## The URL of the nexus repo. Do not change unless you're sure about this.
+remote=vesica/php72
+local=php72prod
+
+## Release tag / version for gg release. If this is not for a specific release, please set this to latest, otherwise set it to a specific release.
+version=1
+
+###########################################################################################
+# UNLESS YOU WANT TO CHANGE SOMETHING TO DO WITH THE PUSH TO NEXUS, LEAVE THE BELOW ALONE #
+###########################################################################################
+## The actual script to build and push the image to nexus
+docker build -f Dockerfile.prod . -t $local
+docker tag $local $remote:$version
+docker push $remote:$version
+if [ "$version" != "latest" ]
+    then
+        docker tag $local $remote:latest
+        docker push $remote:latest
+    fi
