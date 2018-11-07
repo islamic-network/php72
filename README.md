1089888e8214160ea11199b510206f45eecf4629
Meezaan-ud-Din <meezaan@giffgaff.co.uk>
2018-11-07 16:51:45 +0000

    updated to run on openshift online

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# PHP 7.2 Docker Image with Apache
+# PHP 7.2 Docker Image with Apache - OpenShift Online Ready
 To use:
 ```
 docker build -f Dockerfile.dev . -t php
-docker run -d -p 8080:80 -p 443:443 -it --name=php -v $(pwd)/app:/app php
+docker run -d -p 8080:80 -p 443:443 -it --name=php -v $(pwd)/var/www/html:/var/www/html php
 ```
 
 Remember:
 
-Document root in the container is /app/www.
+Document root in the container is /var/www/html.
--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 remote=vesica/php72
 local=php72prod
 
-## Release tag / version for gg release. If this is not for a specific release, please set this to latest, otherwise set it to a specific release.
-version=2
+## Release tag / version. If this is not for a specific release, please set this to latest, otherwise set it to a specific release.
+version=7
 
 ###########################################################################################
 # UNLESS YOU WANT TO CHANGE SOMETHING TO DO WITH THE PUSH TO NEXUS, LEAVE THE BELOW ALONE #
